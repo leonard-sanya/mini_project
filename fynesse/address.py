ac10e5dae371f8cc11f141257d78e24e0a383958
Leonard <lsanya@aimsammi.org>
2025-09-21 10:04:59 +0300

    added the train_underserved_classifier(), evaluate_underserved_classifier() and plot_underserved_confusion_matrix() functions in the address

--- fynesse/address.py
+++ fynesse/address.py
@@ -22,21 +22,6 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 # Set up logging
 logger = logging.getLogger(__name__)
 
-# Here are some of the imports we might expect
-# import sklearn.model_selection  as ms
-# import sklearn.linear_model as lm
-# import sklearn.svm as svm
-# import sklearn.naive_bayes as naive_bayes
-# import sklearn.tree as tree
-
-# import GPy
-# import torch
-# import tensorflow as tf
-
-# Or if it's a statistical analysis
-# import scipy.stats
-
-
 def analyze_data(data: Union[pd.DataFrame, Any]) -> dict[str, Any]:
     """
     Address a particular question that arises from the data.
